523919f366e0ac6a70ad0eb2f8b2b22208857db0
mary <nickb8592@gmail.com>
2021-09-25 19:27:10 +1200

    testing format and added scrollbar

--- code.py
+++ code.py
@@ -1,10 +1,11 @@
 from tkinter import *
 from tkinter import ttk
+
 root = Tk()
 root.title('Julies party hire')
-root.geometry("500x600")
+root.geometry("1000x600")
 
-# add style
+# add style and theme
 style = ttk.Style()
 style.configure("Treeview",
                 background="lightblue",
@@ -12,18 +13,31 @@ style.configure("Treeview",
                 rowheight=25,
                 fieldbackground="silver"
                 )
+# theme
+style.theme_use('default')
 
-#change selected colour
+# change selected colour
 style.map('Treeview',
-background=[('selected', 'grey')])
+          background=[('selected', 'grey')])
 
+# creating a frame
+tree_frame = Frame(root)
+tree_frame.pack(pady=10)
 
-my_tree = ttk.Treeview(root)
+# scrollbar
+tree_scroll = Scrollbar(tree_frame)
+tree_scroll.pack(side=RIGHT, fill=Y)
+
+#creating gui using treeview
+my_tree = ttk.Treeview(tree_frame, yscrollcommand=tree_scroll.set, selectmode="extended")
+my_tree.pack()
+
+#config scroll bar
+tree_scroll.config(command=my_tree.yview)
 
 
 # defining columns
 my_tree['columns'] = ("Full name", "Receipt number", "Item being hired", "Amount of items")
-
 
 # formatting columns
 my_tree.column("#0", width=0, stretch=NO, minwidth=25)
@@ -39,25 +53,31 @@ my_tree.heading("Receipt number", text="Receipt number", anchor=W)
 my_tree.heading("Item being hired", text="Item being hired", anchor=CENTER)
 my_tree.heading("Amount of items", text="Amount", anchor=W)
 
-
-#add data
-data =  [
+# add data
+data = [
     ["mary", 1782, "tables", 78],
     ["john", 3822, "chairs", 125]
 ]
 
-
+# crete striped row tags
+my_tree.tag_configure('oddrow', background="white")
+my_tree.tag_configure('evenrow', background="lightblue")
 
 global count
-count=0
+count = 0
+
 for record in data:
-    my_tree.insert(parent='', index='end', iid=count, text="", values=(record[0], record[1], record[2], record[3]))
+    if 2 == 0:
+        my_tree.insert(parent='', index='end', iid=count, text="", values=(record[0], record[1], record[2], record[3]),
+                       tags=('oddrow',))
+else:
+    my_tree.insert(parent='', index='end', iid=count, text="", values=(record[0], record[1], record[2], record[3]),
+                   tags=('evenrow',))
+
     count += 1
 
-
-
 # adding data
-#my_tree.insert(parent='', index='end', iid=count, text="", values=(record[0], record[1], record[2], record[3]))
+# my_tree.insert(parent='', index='end', iid=count, text="", values=(record[0], record[1], record[2], record[3]))
 # pack to screen
 my_tree.pack(pady=20)
 
@@ -77,7 +97,7 @@ il.grid(row=0, column=2)
 al = Label(add_frame, text="Amount")
 al.grid(row=0, column=3)
 
-#entry boxes
+# entry boxes
 
 name_box = Entry(add_frame)
 name_box.grid(row=1, column=0)
@@ -91,10 +111,12 @@ il_box.grid(row=1, column=2)
 al_box = Entry(add_frame)
 al_box.grid(row=1, column=3)
 
+
 # add record
 def add_record():
     global count
-    my_tree.insert(parent='', index='end', iid=count, text="", values=(name_box.get(), rl_box.get(), il_box.get(), al_box.get()))
+    my_tree.insert(parent='', index='end', iid=count, text="",
+                   values=(name_box.get(), rl_box.get(), il_box.get(), al_box.get()))
     count += 1
 
     # clear boxes
@@ -103,31 +125,29 @@ def add_record():
     il_box.delete(0, END)
     al_box.delete(0, END)
 
-#remove all records
+
+# remove all records
 def remove_all():
     for record in my_tree.get_children():
         my_tree.delete(record)
 
-#remove one selected
+
+# remove one selected
 def remove_one():
     x = my_tree.selection()[0]
     my_tree.delete(x)
 
-#creating buttons
-add_record = Button(root, text="Add record", command=add_record)
-add_record.pack(pady=20)
 
-#remove all
+# creating buttons
+add_record = Button(root, text="Add record", command=add_record)
+add_record.pack(side=LEFT, pady=20, padx=100)
+
+# remove all
 remove_all = Button(root, text="Remove records", command=remove_all)
 remove_all.pack(pady=10)
 
-#remove only one
+# remove only one
 remove_one = Button(root, text="Remove one selected", command=remove_one)
 remove_one.pack(pady=10)
 
-
-
-
-
 root.mainloop()
-
